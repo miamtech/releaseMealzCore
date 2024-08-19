df4d8ca2f701d05babb333a20bd0c0059752e496
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-08-19 15:35:01 +0200

    release 4.1.0

--- Package.swift
+++ Package.swift
@@ -8,19 +8,21 @@ let package = Package(
     defaultLocalization: "fr",
     platforms: [
         .iOS(.v12),
-        ],
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MealzCore",
-            targets: ["MealzCore"]),
+            targets: ["MealzCore"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "MealzCore",
-            path: "Sources/mealzcore.xcframework"
-        )
+            url: "https://github.com/miamtech/MealzCoreRelease/raw/release/4.1.0/mealzcore.zip",
+            checksum: "7d87e2b45568785cac90689d71aecc177502a86ff5e002028871daa636074bb6"
+        ),
     ]
 )
