35a36dbb848cd4e5ada159bf2967c48948234366
Thomas Lemay <thomas.lemay@miam.tech>
2024-11-15 13:52:21 +0100

    update release

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "MealzCore",
-            path: "Sources/mealzcore.xcframework"
+            url: "https://github.com/miamtech/MealzCoreRelease/raw/release/5.2.0/mealzcore.zip",
+            checksum: "599475fbae05f175815e98c4fb0d97a14cefffabbb88e050c1da71d4980ca999"
         )
     ]
 )
